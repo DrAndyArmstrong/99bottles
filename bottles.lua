9bdb53ad513343e03fb91df386b45fac1d66b4d7
Andrew Armstrong <andrewa@cadonix.com>
2014-09-19 16:55:17 +0100

    Fixed obvious mistake in that remaining beers were not being printed correctly.

--- bottles.lua
+++ bottles.lua
@@ -9,12 +9,18 @@ local verse = 	{
 		}
 
 function printVerse(index)
-	print(index .. verse.phrase_one .. ', ' .. index .. verse.phrase_two)
+	local output = index .. verse.phrase_one .. ', ' .. index .. verse.phrase_two  .. '\r\n'
 	if type(index) ~= 'number' then
-		print(verse.phrase_four .. ', ' .. bottles .. verse.phrase_one .. '.\r\n')
+		output = output .. verse.phrase_four .. ', ' .. bottles .. verse.phrase_one
 	else
-		print(verse.phrase_three .. index .. verse.phrase_one .. '.\r\n')
+		local remaining = index - 1
+		output = output .. verse.phrase_three .. remaining .. verse.phrase_one
+		if remaining == 0 then 
+			output = string.gsub(output, "0", "No more")
+			output = string.gsub(output, "bottles", "bottle", 2)
+		end
 	end
+	print(output  .. '.\r\n')
 end
 
 for i=bottles,1, -1 do
